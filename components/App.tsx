7f8040265aa45289fa3434baf6d60b403f752ede
Elijah Cobb <elijah@elijahcobb.com>
2021-09-17 19:21:14 -0400

    data

--- components/App.tsx
+++ components/App.tsx
@@ -19,54 +19,10 @@ export interface AppProps {
 
 export const App: FC<AppProps> = props => {
 
-	const [markdown, setMarkdown] = useState(`# header 1
-## header 2
-### header 3
-#### header 4
-##### header 5
-
-This is a paragraph... A paragraph can have \`code\` inside of it, but it can also have
-functions provided by Latex $y=mx+b$. You can have **bold** text, or *italic* text,
-or ~strikethrough~ text. It all works rather nicely.
-
-\`\`\`typescript
-function handleSourceCode(): boolean {
-    // with syntax highlighting
-    return true;
-}
-\`\`\`
-
-|Col1|Col2|Col3|
----|---|---
-|11|12|13|
-|21|22|23|
-|31|32|33|
-
-[Link to website](google.com)
-
-* so this is good
-* this is also good
-* ok what about this
-    * ok well this one is indented
-    * and this one is too
-    * so it Thi
-* but this one is back to normal
-
-1. here is a numbered list
-1. yeah see it keeps going
-1. yep more
-    * whoa an indent off this one
-    * yep
-1. and back
-
-**tasks**
-- [ ] get this working
-- [ ] ok wow cool
-- [x] be cool
-`);
+	const [markdown, setMarkdown] = useState(props.data);
 
 	function save() {
-		var xhr = new XMLHttpRequest();
+		const xhr = new XMLHttpRequest();
 		xhr.open("POST", '/api/update', true);
 		xhr.setRequestHeader("Content-Type", "application/json");
 		xhr.onreadystatechange = function() {
