fe7c310cf19b4842ac3bacab8d254310fbdecd57
Elijah Cobb <elijah@elijahcobb.com>
2021-09-17 19:11:54 -0400

    data

--- pages/api/fetch.ts
+++ pages/api/fetch.ts
@@ -8,7 +8,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import {getAuthFromCookie} from "../../api/google";
 import {google} from "googleapis";
-import {PdMethod, PdRequest} from "@element-ts/palladium";
 
 export default async function handler(
 	req: NextApiRequest,
@@ -22,26 +21,14 @@ export default async function handler(
 
 	res.send("ajj");
 
-	// const file = await drive.files.get({fileId, fields: "*"})
-	//
-	// console.log(file);
-	//
-	// //@ts-ignore
-	// const name = file.data.owners[0].displayName as string;
-	// //@ts-ignore
-	// const profile = file.data.owners[0].photoLink as string;
-	// const fileName = (file.data.name as string).replace(".md", "");
-	// const dataUrl = file.data.webContentLink as string;
-	//
-	// const response = await fetch(dataUrl);
-	// const data = await response.text();
-	//
-	// res.send({name, profile, fileName, data});
-	// // // @ts-ignore
-	// // const name = file.data.name?.replace(".md", "");
-	// // res.send(JSON.stringify({
-	// // 	name,
-	// // 	// @ts-ignore
-	// // 	data: Buffer.from(file.data).toString("utf-8")
-	// // }));
+	const fileInfo = await drive.files.get({fileId, fields: "*"})
+	//@ts-ignore
+	const name = fileInfo.data.owners[0].displayName as string;
+	//@ts-ignore
+	const profile = fileInfo.data.owners[0].photoLink as string;
+	const fileName = (fileInfo.data.name as string).replace(".md", "");
+
+	const fileData = await drive.files.get({fileId, alt: "media"})
+
+	res.send({name, profile, fileName, data: fileData.data});
 }
