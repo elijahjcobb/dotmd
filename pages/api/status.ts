162e51f434dcba2bec5f67986e2e9c79b7a47ec6
Elijah Cobb <elijah@elijahcobb.com>
2021-09-17 17:55:32 -0400

    test

---
+++ pages/api/status.ts
@@ -0,0 +1,15 @@
+/**
+ * Elijah Cobb
+ * elijah@elijahcobb.com
+ * elijahcobb.com
+ * github.com/elijahjcobb
+ */
+
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse
+) {
+	res.send(req.cookies);
+}
