db32ac41de101d7d1c67314a82557f326e6f4229
Elijah Cobb <elijah@elijahcobb.com>
2021-09-17 18:12:59 -0400

    fetch

---
+++ pages/api/fetch.ts
@@ -0,0 +1,22 @@
+/**
+ * Elijah Cobb
+ * elijah@elijahcobb.com
+ * elijahcobb.com
+ * github.com/elijahjcobb
+ */
+
+import type { NextApiRequest, NextApiResponse } from "next";
+import {getAuthFromCookie} from "../../api/google";
+import {google} from "googleapis";
+
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse
+) {
+	const auth = getAuthFromCookie(req.cookies);
+	const drive = google.drive({version: "v3", auth});
+	const fileId = req.cookies.file
+
+	const file = await drive.files.get({fileId})
+	res.send(JSON.stringify(file));
+}
