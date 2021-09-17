0df09fc31bc453a774bbeea31c6fb5eadc7dc23f
Elijah Cobb <elijah@elijahcobb.com>
2021-09-17 18:33:54 -0400

    data

--- pages/api/fetch.ts
+++ pages/api/fetch.ts
@@ -17,7 +17,7 @@ export default async function handler(
 	const drive = google.drive({version: "v3", auth});
 	const fileId = req.cookies.file
 
-	const file = drive.files.get({fileId, alt: 'media'})
+	const file = await drive.files.get({fileId, alt: 'media'})
 	res.send(file);
 	// // @ts-ignore
 	// const name = file.data.name?.replace(".md", "");
