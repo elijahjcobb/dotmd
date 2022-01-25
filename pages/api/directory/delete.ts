f4d8ed3b91424da7d65ce8f613661d1a19c4378d
Elijah Cobb <ejcobb@mtu.edu>
2022-01-25 09:41:27 -0500

    deleting dir recursion

--- pages/api/directory/delete.ts
+++ pages/api/directory/delete.ts
@@ -7,21 +7,27 @@
 
 import type { NextApiRequest, NextApiResponse } from 'next'
 import {getUserFromAuth} from "../../../db/auth-silicon";
-import {Directory} from "../../../db/DB";
+import {Directory, File} from "../../../db/DB";
 import {SiQuery} from "@element-ts/silicon";
 import {ObjectId} from "bson";
+import {deleteFile} from "../file/delete";
+
+export async function deleteDirectory(dir: Directory): Promise<void> {
+	const files = await (new SiQuery(File, {parent: dir.getId()})).getAll();
+	const dirs = await (new SiQuery(Directory, {parent: dir.getId()})).getAll();
+	for (const f of files) await deleteFile(f);
+	for (const d of dirs) await deleteDirectory(d);
+	await dir.delete();
+
+}
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
 	const user = await getUserFromAuth(req);
 	if (!user) return res.status(400).send("Not authorized.");
-
-	const {id, name} = req.query as {id: string, name: string};
-
+	const {id} = req.query as {id: string};
 	const dir = await (new SiQuery(Directory, {_id: new ObjectId(id), owner: new ObjectId(user.getHexId())})).getFirst();
 	if (!dir) return res.status(404).send("Dir dne.");
-
-	dir.put("name", name);
-	await dir.save();
+	await deleteDirectory(dir);
 
 	res.json(dir.toJSON());
 
--- pages/api/file/delete.ts
+++ pages/api/file/delete.ts
@@ -11,6 +11,11 @@ import {SiQuery} from "@element-ts/silicon";
 import {Attachment, File} from "../../../db/DB";
 import {ObjectId} from "bson";
 
+export async function deleteFile(file: File): Promise<void> {
+	await file.delete();
+	const attachments = await (new SiQuery(Attachment, {parent: file.getId()})).getAll();
+	for (const a of attachments) await a.delete();
+}
 
 export default async function handler(
 	req: NextApiRequest,
@@ -22,10 +27,7 @@ export default async function handler(
 	const query = new SiQuery(File, {_id: new ObjectId(id)});
 	const file = await query.getFirst();
 	if (!file || file.get("owner") !== user.getHexId())return res.status(400).send("Not authorized.");
-	await file.delete();
-
-	const attachments = await (new SiQuery(Attachment, {parent: file.getId(), owner: user.getId()})).getAll();
-	for (const a of attachments) await a.delete();
+	await deleteFile(file);
 
 	res.send({status: "Deleted"});
 }
