79105cc2203e3b1da1227ca7377c6e454ba0bbe0
Elijah Cobb <ejcobb@mtu.edu>
2022-01-25 09:37:47 -0500

    added page delete

---
+++ pages/api/file/delete.ts
@@ -0,0 +1,31 @@
+/**
+ * Elijah Cobb
+ * elijah@elijahcobb.com
+ * elijahcobb.com
+ * github.com/elijahjcobb
+ */
+
+import type {NextApiRequest, NextApiResponse} from "next";
+import {getUserFromAuth} from "../../../db/auth-silicon";
+import {SiQuery} from "@element-ts/silicon";
+import {Attachment, File} from "../../../db/DB";
+import {ObjectId} from "bson";
+
+
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse
+) {
+	const user = await getUserFromAuth(req);
+	if (!user) return res.status(400).send("Not authorized.");
+	const {id} = req.body as {content: string, id: string, name: string};
+	const query = new SiQuery(File, {_id: new ObjectId(id)});
+	const file = await query.getFirst();
+	if (!file || file.get("owner") !== user.getHexId())return res.status(400).send("Not authorized.");
+	await file.delete();
+
+	const attachments = await (new SiQuery(Attachment, {parent: file.getId(), owner: user.getId()})).getAll();
+	for (const a of attachments) await a.delete();
+
+	res.send({status: "Deleted"});
+}
